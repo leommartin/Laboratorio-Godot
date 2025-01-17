447c8f0b6cd88c1251b80bffd0d83a90116effcb
Leonardo Martin <leowork2002@gmail.com>
2025-01-17 15:03:24 -0400

    arrumando animação do personagem cima/baixo

--- laboratorio-godot/player.gd
+++ laboratorio-godot/player.gd
@@ -22,8 +22,15 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("ui_left"):
 		$AnimatedSprite2D.play("andando_tras")
+		
 	if Input.is_action_just_pressed("ui_right"):
 		$AnimatedSprite2D.play("andando_frente")
+		
+	if Input.is_action_just_pressed("ui_up"):
+		$AnimatedSprite2D.play("andando_cima")
+		
+	if Input.is_action_just_pressed("ui_down"):
+		$AnimatedSprite2D.play("andando_baixo")
 	
 			
 		
